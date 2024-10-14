22b050e9271657b694e47e555b6a6b4af914d924
Mikhail <michael@roofbuddy.co.nz>
2024-10-14 22:31:02 +0300

    :feat: add userinput

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,20 @@
-const args = process.argv.slice(2)
-const userNameArg = args.find(arg => arg.startsWith('--username='))
+import os from 'os';
+import process from 'process';
+import { startCLI } from './cli.js';
+
+const homeDirectory = os.homedir();
+process.chdir(homeDirectory);
+
+const args = process.argv.slice(2);
+const userNameArg = args.find((arg) => arg.startsWith('--username='));
 const userName = userNameArg ? userNameArg.split('=')[1] : 'Guest';
 
-if (userName) {
-  console.log(`Welcome to the File Manager, ${userName}`);
-} else {
-  console.log('Welcome to the File Manager!');
-}
+console.log(`Welcome to the File Manager, ${userName}!`);
+console.log(`You are currently in ${process.cwd()}`);
+
+process.on('SIGINT', () => {
+	console.log(`\nThank you for using File Manager, ${userName}, goodbye!`);
+	process.exit();
+});
+
+startCLI(userName);
